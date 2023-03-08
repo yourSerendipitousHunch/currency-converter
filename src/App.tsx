6b4cc8f5c27454d6d6c81f1e6b55d6126423e99e
Anton <krava34007@gmail.com>
2023-03-08 23:37:41 +0200

    conversion occur in one direction

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Header} from './components/Header';
-import {Main} from './components/Main';
+import Main from './components/Main';
 import {Footer} from './components/Footer';
 
 function App() {
   return (
     <div className='wrapper'>
     <Header/>
-    <Main/>
+    <main><Main/></main>
     <Footer/>
     </div>
   )
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,29 +1,79 @@
-export function Main() {
+import React, { useState, useEffect } from "react";
+
+interface ExchangeRate {
+  [key: string]: number;
+  USD: number;
+  EUR: number;
+  UAH: number;
+}
+
+export default function CurrencyConverter() {
+  const [exchangeRate, setExchangeRate] = useState<ExchangeRate>({
+    USD: 0,
+    EUR: 0,
+    UAH: 0,
+  });
+  const [currency1, setCurrency1] = useState<string>('USD');
+  const [currency2, setCurrency2] = useState<string>('UAH');
+  const [amount, setAmount] = useState<number>(0);
+  const [convertedAmount, setConvertedAmount] = useState<number>(0);
+  const [error, setError] = useState<Error | undefined>(undefined);
+
+  useEffect(() => {
+    const getExchangeRate = async () => {
+      try {
+        const response = await fetch(
+          'https://openexchangerates.org/api/latest.json?app_id=951c25ec7e2643d29f0f7ed23fa0ff10'
+        );
+        const data = await response.json();
+        setExchangeRate(data.rates);
+      } catch (err: unknown) {
+        setError(err as Error);
+      }
+    };
+
+    getExchangeRate();
+  }, []);
+
+  function handleCurrency1Change(event: React.ChangeEvent<HTMLSelectElement>) {
+    setCurrency1(event.target.value);
+    setConvertedAmount(amount / exchangeRate[event.target.value] * exchangeRate[currency2]);
+  }
+
+  function handleCurrency2Change(event: React.ChangeEvent<HTMLSelectElement>) {
+    setCurrency2(event.target.value);
+    setConvertedAmount(amount / exchangeRate[currency1] * exchangeRate[event.target.value]);
+  }
+
+  function handleAmountChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setAmount(parseFloat(event.target.value));
+    setConvertedAmount(parseFloat(event.target.value) / exchangeRate[currency1] * exchangeRate[currency2]);
+  }
+
+  function handleConvert() {
+    setConvertedAmount(amount / exchangeRate[currency1] * exchangeRate[currency2]);
+  }
+
   return (
-    <main>
-      <div className="container">
-        <div className="main__page">
-        <article>
-          <div className="currency-select">
-          <select>
-            <option value="1">USD</option>
-            <option value="2">EUR</option>
-            <option value="3">UAH</option>
-          </select>
-          <input name="other" value=""/>
-          </div>
-          <div className="currency-select">
-          <select>
-            <option value="1">USD</option>
-            <option value="2">EUR</option>
-            <option value="3">UAH</option>
-          </select>
-          <input name="other" value=""/>
-          </div>
-        </article>
-        <button>Conversion</button>
-        </div>
+    <div>
+      <div className="currency-select">
+        <select value={currency1} onChange={handleCurrency1Change}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="UAH">UAH</option>
+        </select>
+        <input type="number" value={amount} onChange={handleAmountChange} />
       </div>
-    </main>
-  )
+      <div className="currency-select">
+        <select value={currency2} onChange={handleCurrency2Change}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="UAH">UAH</option>
+        </select>
+        <input type="number" value={convertedAmount} readOnly />
+      </div>
+      <button onClick={handleConvert}>Convert</button>
+      {error && <div>Failed to fetch exchange rates: {error.message}</div>}
+    </div>
+  );
 }
