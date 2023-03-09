af56660ffea1b5b27265ec0075ea660bf9a9d0f3
Anton <krava34007@gmail.com>
2023-03-09 10:33:00 +0200

    correctly working app

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 interface ExchangeRate {
   [key: string]: number;
@@ -8,38 +8,60 @@ interface ExchangeRate {
 }
 
 export default function CurrencyConverter() {
-  const [exchangeRate] = useState<ExchangeRate>({
+  const [exchangeRate, setExchangeRate] = useState<ExchangeRate>({
     USD: 1,
-    EUR: 0.85,
-    UAH: 27.7,
+    EUR: 0,
+    UAH: 0,
   });
   const [currency1, setCurrency1] = useState<string>("USD");
   const [currency2, setCurrency2] = useState<string>("UAH");
   const [amount1, setAmount1] = useState<number>(0);
   const [amount2, setAmount2] = useState<number>(0);
 
+  useEffect(() => {
+    fetch(
+      "https://openexchangerates.org/api/latest.json?app_id=951c25ec7e2643d29f0f7ed23fa0ff10"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const usdToUah = data.rates.UAH;
+        const usdToEur = data.rates.EUR / data.rates.USD;
+        const uahToUsd = 1 / usdToUah;
+        const eurToUsd = 1 / usdToEur;
+        setExchangeRate({
+          USD: 1,
+          EUR: eurToUsd,
+          UAH: uahToUsd,
+        });
+      });
+  }, []);
+
   const handleAmount1Change = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseFloat(event.target.value);
     setAmount1(value);
-    setAmount2(value * exchangeRate[currency1] / exchangeRate[currency2]);
+    setAmount2((value * exchangeRate[currency1]) / exchangeRate[currency2]);
   };
 
   const handleAmount2Change = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseFloat(event.target.value);
     setAmount2(value);
-    setAmount1(value * exchangeRate[currency2] / exchangeRate[currency1]);
+    setAmount1((value * exchangeRate[currency2]) / exchangeRate[currency1]);
   };
 
-  const handleCurrency1Change = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleCurrency1Change = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const value = event.target.value;
     setCurrency1(value);
-    setAmount2(amount1 * exchangeRate[value] / exchangeRate[currency2]);
+    setAmount2((amount1 * exchangeRate[value]) / exchangeRate[currency2]);
   };
 
-  const handleCurrency2Change = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleCurrency2Change = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const value = event.target.value;
     setCurrency2(value);
-    setAmount2(amount1 * exchangeRate[currency1] / exchangeRate[value]);
+    setAmount2((amount1 * exchangeRate[currency1]) / exchangeRate[value]);
   };
 
   return (
